2822924298cfeb9d5d2a5d49fab65a593409e4a0
Bernardo Bonanca <bernardo@unlockingthedigital.com>
2017-12-14 11:25:17 +0000

    added post list component

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,33 @@
   <div id="app">
     <h1>Hello Withch1ban!</h1>
 
-    <Post
-      title="This is a post title"
-      description="This is a post description"
-      votes="123"
-    />
-  </div>
+    <PostList :posts="posts"/>
+</div>
 </template>
 
 <script>
-  import Post from './components/post/Post.vue';
+  import PostList from './components/post-list/Post-list.vue';
 
   export default {
     name: 'App',
     components: {
-      Post
+      PostList
+    },
+    data: () => {
+      return {
+        posts: [
+          {
+            title: 'hello',
+            description: 'description1',
+            votes: 5000
+          },
+          {
+            title: 'Street Fighter',
+            description: 'description2',
+            votes: 100000
+          }
+        ]
+      }
     }
   }
 </script>
