5557319052143e7ecf70ef60830edc75ca7dd705
Bernardo Bonanca <bernardo@unlockingthedigital.com>
2017-12-14 16:43:50 +0000

    improved variables

--- src/utils/user-id/index.js
+++ src/utils/user-id/index.js
@@ -1,6 +1,6 @@
 const uuid = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 };
@@ -27,9 +27,9 @@ const getUserIdFromCookie = () => {
 
 const generateUserIdCookie = () => {
   const id = uuid();
-  const d = new Date();
-  d.setTime(d.getTime() + (365*24*60*60*1000));
-  const expires = "expires="+ d.toUTCString();
+  const date = new Date();
+  date.setTime(date.getTime() + (365*24*60*60*1000));
+  const expires = "expires="+ date.toUTCString();
   document.cookie = "userId=" + id + ";" + expires + ";path=/";
   return id;
 }
