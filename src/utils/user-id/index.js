fcef3d7cf05e18d020feb77bb8a54177a2b9282b
Bernardo Bonanca <bernardo@unlockingthedigital.com>
2017-12-14 16:32:50 +0000

    removed testing, will need refactoring

--- src/utils/user-id/index.js
+++ src/utils/user-id/index.js
@@ -1,11 +1,11 @@
-export const uuid = () => {
+const uuid = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 };
 
-export const getUserIdFromCookie = () => {
+const getUserIdFromCookie = () => {
   const name = "userId=";
   const decodedCookie = decodeURIComponent(document.cookie);
   const ca = decodedCookie.split(';');
@@ -25,7 +25,7 @@ export const getUserIdFromCookie = () => {
   return "";
 }
 
-export const generateUserIdCookie = () => {
+const generateUserIdCookie = () => {
   const id = uuid();
   const d = new Date();
   d.setTime(d.getTime() + (365*24*60*60*1000));
@@ -34,6 +34,6 @@ export const generateUserIdCookie = () => {
   return id;
 }
 
-export const userId = getUserIdFromCookie() ? getUserIdFromCookie() : generateUserIdCookie();
+const userId = getUserIdFromCookie() ? getUserIdFromCookie() : generateUserIdCookie();
 
 export default userId;
--- src/utils/user-id/user-id.test.js
+++
@@ -1,1 +0,0 @@
-import userId from './index.js'
