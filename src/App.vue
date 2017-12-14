92ce44ec9aebf470f31811ad7805e84a6ea227a6
Bernardo Bonanca <bernardo@unlockingthedigital.com>
2017-12-14 10:49:21 +0000

    added post component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <h1>Hello Withch1ban!</h1>
+  <div id="app">
+    <h1>Hello Withch1ban!</h1>
+
+    <Post
+      title="This is a post title"
+      description="This is a post description"
+      votes="123"
+    />
+  </div>
 </template>
 
 <script>
+  import Post from './components/post/Post.vue';
+
   export default {
-    name: 'App'
+    name: 'App',
+    components: {
+      Post
+    }
   }
 </script>
-
-<style lang="scss">
-  body {
-    background-color: #c1c1c1;
-  }
-</style>
