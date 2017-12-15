4fc57d0217bf0eb5e8b51b804f24311daeb0929e
tonyle-wb <32737813+tonyle-wb@users.noreply.github.com>
2017-12-15 12:37:53 +0000

    Add styles for frontend post (#13)
    * fonts
    
    * list styling
    
    * logo
    
    * styling
    
    * Removed restaurants, fixed minor styling issue
    
    * whitbread logo
    
    * list styling
    
    * logo
    
    * styling
    
    * Removed restaurants, fixed minor styling issue
    
    * added voting up and down
    
    * whitbread logo
    
    * handling changing a vote from up to down or vice versa and update view
    
    * Changes made as per comments
    
    * Removed fonts - they weren't working

--- src/components/post/Post.vue
+++ src/components/post/Post.vue
@@ -1,18 +1,176 @@
 <template lang="html">
-  <div class="post">
-    <h2 class="post__title">{{title}}</h2>
-    <p class="post__description">{{description}}</p>
-    <p class="post__votes">{{votes}}</p>
+  <div class="post xs-col-12" :class="[brand ? brand : '']">
+    <div class="post__logo xs-col-2">
+      <div v-if="brand === 'pi'">
+        <img class="brand-logo" src="../../img/logo-premierinn.png" />
+      </div>
+      <div v-else-if="brand === 'costa'">
+        <img class="brand-logo" src="../../img/logo-costa.png" />
+      </div>
+      <div v-else-if="brand === 'hub'">
+        <img class="brand-logo" src="../../img/logo-hub.png" />
+      </div>
+    </div>
+    <div class="post__details-container xs-col-8">
+      <h2 class="post__title">{{title}}</h2>
+      <p class="post__description">{{description}}</p>
+    </div>
+    <div class="post__votes-container xs-col-2">
+      <span class="vote vote--up" @click="upvote(postId)"></span>
+      <p class="post__votes">{{votes}}</p>
+      <span class="vote vote--down" @click="downvote(postId)"></span>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import userId from '../../utils/user-id';
+
 export default {
   name: 'Post',
+
   props: [
     'title',
     'description',
-    'votes'
-  ]
+    'votes',
+    'brand',
+    'postId'
+  ],
+
+  data() {
+    return {
+      voteState: 'none'
+    }
+  },
+
+  methods: {
+    upvote(postId) {
+      axios.post('http://innovation-vote.whitbread.digital:8080/vote', {
+        userId,
+        postId,
+        vote: 'up'
+      })
+      .then((response) => {
+          this.$emit('upvote', {
+            id: this.postId,
+            voteState: this.voteState
+          });
+          this.voteState = 'up';
+      })
+      .catch(function (error) {
+          console.log(error.message);
+      })
+    },
+
+    downvote(postId) {
+      axios.post('http://innovation-vote.whitbread.digital:8080/vote', {
+        userId,
+        postId,
+        vote: 'down'
+      })
+      .then((response) => {
+          this.$emit('downvote', {
+            id: this.postId,
+            voteState: this.voteState
+          });
+          this.voteState = 'down';
+      })
+      .catch(function (error) {
+          console.log(error.message);
+      })
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+
+body{
+  font-family: 'Arial';
+}
+
+.logo{
+  width:150px;
+  padding: 20px;
+}
+
+.post{
+  float: left;
+  font-family: 'Arial';
+
+    h2{
+    text-align: left;
+    margin-top: 0;
+  }
+}
+
+.post__logo{
+  float: left;
+}
+
+.post-item{
+  float: left;
+  padding: 10px 20px;
+  box-sizing: border-box;
+}
+
+.post__list{
+  background-color: #f7f7f7;
+  float: left;
+  margin-top: 30px;
+}
+
+.post__details-container{
+  float: left;
+  padding: 0 20px;
+box-sizing: border-box;
+}
+
+.post__votes-container{
+  float: left;
+  font-size: 18px;
+  text-align: center;
+
+
+}
+
+.vote{
+  width: 40px;
+  height: 30px;
+  background-size: 100%;
+  display: inline-block;
+  text-align: center;
+  cursor: pointer;
+}
+
+.vote--up {
+  background-image: url("../../img/sort-up.svg");
+  fill: #ff0000;
+}
+
+.vote--down {
+  background-image: url("../../img/sort-down.svg");
+  fill: #ff0000;
+}
+
+.pi{
+  color: #511e62
+}
+
+.hub{
+  color: #bed600
+}
+
+.costa{
+  color: #5d301b
+}
+
+.brand-logo{
+  max-width: 100px;
+width: 100%;
+float: left;
+}
+
+
+</style>
